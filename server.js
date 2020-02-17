997702af997a9e3bdd33279ad322be530ed1cc12
Yutian <yutiancoding@gmail.com>
2020-02-17 16:02:53 -0500

    index file

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const restRouter = require('./routes/rest');
+const indexRouter = require('./routes/index');
 const mongoose = require('mongoose');
+const path = require('path');
+
 mongoose.connect('mongodb://yutianz:zyt930615@ds145304.mlab.com:45304/1805')
 
 //app.get('/', (req, res) => res.send('Hello World '))
+app.use(express.static(path.join(__dirname, '../public')));
 app.use('', restRouter);
 //see express doc for detail
 
