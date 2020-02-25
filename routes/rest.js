9fb31ff393a6f63fae9484fd04302c821da975b9
Yutian <yutiancoding@gmail.com>
2020-02-25 18:37:16 -0500

    executor

--- routes/rest.js
+++ routes/rest.js
@@ -1,30 +1,66 @@
 const express = require('express');
 const router = express.Router();
+
+
+const problemService = require('../services/problemService');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
-const problemService = require('../services/problemService');
+const nodeRestClient = require('node-rest-client').Client;
 
-router.get('/problems', (req, res) => {
-    problemService.getProblems()
-        .then(problems => res.json(problems))
-})
+const restClient = new nodeRestClient();
 
-//get single problem
-router.get('/problems/:id', (req, res) => {
-    const id = req.params.id;
-    problemService.getProblem(+id)
-        .then(problem => res.json(problem));
-})
+EXECUTOR_SERVER_URL = 'http://localhost:5000/build_and_run';
 
-//add a problem
-router.post('/problems', jsonParser, (req, res) => {
-    problemService.addProblem(req.body)
-        .then(problem => {
-            res.json(problem);
-        }, error => {
-            res.status(400).send("Problem already exists.");
-        });
+restClient.registerMethod('build_and_run', EXECUTOR_SERVER_URL, 'POST');
+
+router.get('/problems', function(req, res) {
+	problemService.getProblems()
+		.then(problems => res.json(problems));
+});
+
+
+router.get('/problems/:id', function(req, res) {
+	const id = req.params.id;
+	problemService.getProblem(+id)
+		.then(problem => res.json(problem));
+});
+
+
+router.post('/problems', jsonParser, function(req, res) {
+	problemService.addProblem(req.body)
+		.then(problem => {
+			res.json(problem);
+		}, error => {
+			res.status(400).send('Problem name already exists!');
+		});
+});
+
+router.post('/build_and_run', jsonParser, function(req, res) {
+	const code = req.body.code;
+	const lang = req.body.lang;
+
+	console.log('lang: ', lang, 'code: ', code);
+
+	restClient.methods.build_and_run(
+	{
+		data: {code: code, lang: lang},
+		headers: {'Content-Type': 'application/json'}
+	},
+	(data, response) => {
+		const text = `Build output: ${data['build']}, execute output: ${data['run']}`;
+		res.json(text);
+	}
+	)
+});
+
+router.put('/problems', jsonParser, function(req, res) {
+	problemService.modifyProblem(req.body)
+		.then(problem => {
+			res.json(problem);
+		}, error => {
+			res.status(400).send('Failed to update problem');
+		});
 })
 
 module.exports = router;
