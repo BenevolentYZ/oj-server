379d0f55eaab16bb1ce929be042cf18a1a7db331
Yutian <yutiancoding@gmail.com>
2020-02-24 19:51:23 -0500

    socket.io add

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const restRouter = require('./routes/rest');
 const indexRouter = require('./routes/index');
 const mongoose = require('mongoose');
 const path = require('path');
+var http = require('http');
+var socketIO = require('socket.io');
+var io = socketIO();
+var editorSocketService = require('./services/editorSocketService')(io);
 
 mongoose.connect('mongodb://yutianz:zyt930615@ds145304.mlab.com:45304/1805')
 
@@ -13,4 +17,14 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('', restRouter);
 //see express doc for detail
 
-app.listen(port, () => console.log(`running on ${port}`))
+//app.listen(port, () => console.log(`running on ${port}`))
+const server = http.createServer(app);
+io.attach(server);
+server.listen(port);
+server.on('listening', () => {
+    console.log('App is listening to port 3000');
+});
+
+app.use((req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, '../public')});
+})
